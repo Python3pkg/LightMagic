323ddd233b7112d64b3ee833aa714d5fc5ab5339
Maxim Radyukov <maxim.radyukov@gmail.com>
2017-03-01 20:58:25 +0300

    Change Enum logic. version: 0.2.0

--- LightMagic/types/Enum.py
+++ LightMagic/types/Enum.py
@@ -6,9 +6,10 @@ class Enum(_Base):
         Работа с str
     """
 
-    def __init__(self, list_of_values=None, *args, **kwargs):
+    def __init__(self, list_of_values=None, formatter=str, *args, **kwargs):
         # Список допустимых значений
-        self.list_of_values = list_of_values
+        self.formatter = formatter
+        self.list_of_values = [formatter(x) for x in list_of_values]
 
         super().__init__(*args, **kwargs)
 
@@ -16,7 +17,9 @@ class Enum(_Base):
         """
             Проверяем корректность входных данных
         """
+        value = self.formatter(value)
         if value not in self.list_of_values:
+            print(self.list_of_values)
             raise ValueError('Invalid value "%s"' % value)
 
         return value
